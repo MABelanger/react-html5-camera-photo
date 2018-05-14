9e247128a279bdac999f0cf36886b827a165d253
Michel-Alexandre Belanger <mabelanger@solutionsmedias360.com>
2018-05-14 19:56:17 -0400

    base64 click.mp3

--- src/lib/components/Camera/index.js
+++ src/lib/components/Camera/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import LibCameraPhoto from 'jslib-html5-camera-photo';
 import CircleButton from '../CircleButton';
 import {getShowHideStyle} from './helpers';
-// import click from './data/click.base64.json';
+import click from './data/click.base64.json';
 
 // import StopStartButton from '../StopStartButton';
 
@@ -74,11 +74,7 @@ class Camera extends React.Component {
   }
 
   playClickAudio () {
-    // let clickBinary = window.atob(click.base64);
-    // console.log('clickBinary', clickBinary);
-    // decode base64 to binary
-    // pass it to Audio
-    let audio = new Audio('click.mp3');
+    let audio = new Audio('data:audio/mp3;base64,' + click.base64);
     audio.play();
   }
 
