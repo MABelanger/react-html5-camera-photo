130c1dd7f72effcadddf904aac527028740a7d85
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2018-07-25 20:32:35 -0400

    simplify AppWithDynamicProperties.js

--- src/demo/AppWithDynamicProperties.js
+++ src/demo/AppWithDynamicProperties.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Camera, { FACING_MODES, IMAGE_TYPES } from '../lib';
+import Camera, { FACING_MODES } from '../lib';
 import './reset.css';
 
 /*
@@ -12,27 +12,16 @@ class App extends Component {
     this.state = {
       idealFacingMode: FACING_MODES.ENVIRONMENT
     };
+    this.renderButtons = this.renderButtons.bind(this);
   }
   onTakePhoto (dataUri) {
     // Do stuff with the photo...
     console.log('takePhoto');
   }
 
-  onCameraError (error) {
-    console.error('onCameraError', error);
-  }
-
-  onCameraStart (stream) {
-    console.log('onCameraStart');
-  }
-
-  onCameraStop () {
-    console.log('onCameraStop');
-  }
-
-  render () {
+  renderButtons () {
     return (
-      <div className="App">
+      <div>
         <button onClick={ (e) => {
           this.setState({idealFacingMode: FACING_MODES.USER});
         }}> FACING_MODES.USER </button>
@@ -40,19 +29,17 @@ class App extends Component {
         <button onClick={ (e) => {
           this.setState({idealFacingMode: FACING_MODES.ENVIRONMENT});
         }}> FACING_MODES.ENVIRONMENT </button>
+      </div>
+    );
+  }
 
+  render () {
+    return (
+      <div className="App">
+        { this.renderButtons() }
         <Camera
           onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
-          onCameraError = { (error) => { this.onCameraError(error); } }
           idealFacingMode = {this.state.idealFacingMode}
-          idealResolution = {{width: 640, height: 480}}
-          imageType = {IMAGE_TYPES.JPG}
-          imageCompression = {0.97}
-          isMaxResolution = {false}
-          isImageMirror = {false}
-          sizeFactor = {1}
-          onCameraStart = { (stream) => { this.onCameraStart(stream); } }
-          onCameraStop = { () => { this.onCameraStop(); } }
         />
       </div>
     );
