2eda967bfc675668047778da79e84f826d7fa093
Michel-Alexandre Belanger <mabelanger@solutionsmedias360.com>
2018-07-26 13:41:45 -0400

    AppMinimumUsage by default

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import App from './AppMinimumUsage';
+import App from './AppMinimumUsage';
 // import App from './AppAllProps';
-import App from './AppWithUmountCamera';
+// import App from './AppWithUmountCamera';
 // import App from './AppWithDynamicProperties';
 
 ReactDOM.render(<App />, document.getElementById('root'));
