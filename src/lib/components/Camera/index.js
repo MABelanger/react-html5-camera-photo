17e6497de174e23ffb54ddd9f1b7312a9ff45d4c
ron dembo <>
2018-07-26 16:33:36 +0300

    Adding playsInline param (for IOS support)

--- src/lib/components/Camera/index.js
+++ src/lib/components/Camera/index.js
@@ -158,6 +158,7 @@ class Camera extends React.Component {
           style = {videoStyles}
           ref={this.videoRef}
           autoPlay="true"
+          playsInline
         />
         {this.renderCircleButton()}
       </div>
